91056810d8154980e46e0f829b45004c67599ae4
zami26 <arafatulislamzami@gmail.com>
2023-01-04 00:36:54 +0600

    third commit

--- src/components/Header/shop/shop.js
+++ src/components/Header/shop/shop.js
@@ -9,6 +9,12 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
+    const [card, setCard] = useState([]);
+    const handelAddToCard = (product) => {
+        console.log(product);
+        const newCard = [...card, product];
+        setCard(newCard);
+    }
     return (
         <div className='shop-container'>
             <div className='card-container'>
@@ -16,12 +22,14 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handelAddToCard={handelAddToCard}
                     ></Product>)
                 }
 
             </div>
             <div className='order-container'>
                 <h4>Order Summary</h4>
+                <p>Selected items:{card.length}</p>
             </div>
         </div>
     );
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css'
 
 const Product = (props) => {
@@ -8,12 +11,13 @@ const Product = (props) => {
             <img src={img} alt="" />
             <div className='product-info'>
                 <p className='product-name'>{name}</p>
-                <p>Price: {price}</p>
+                <p>Price: ${price}</p>
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings}</small></p>
             </div>
             <button className='card-btn'>
-                <p>Add to Card</p>
+                <p onClick={() => { props.handelAddToCard(props.product) }}>Add to Card</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
